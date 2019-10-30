5c19db1fda1aab807180848ba7abef25c1f72a6a
Jadd Cheng <52934066+jlcatx512@users.noreply.github.com>
2019-10-30 16:57:50 -0500

    Finished main functionality of app.js code. Successfully renders table on load. Need to fix filteredData function and how to deal with rendering new table.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # javascript-challenge
-October 28, 2019
+* October 28, 2019
+* https://utmcbdatapt07-jxr4493.slack.com/files/UL3B0JR2P/FPJGFQCCR/activity_9_app.js?origin_team=TL645CUB0
+    * Really helpful.
 
 ## pseudocode ##
 * Define button object.
@@ -11,6 +13,25 @@ October 28, 2019
 ## Define button object ##
 * What is `id` attribute of the button?
     * `id="filter-btn"`
+
+[Free D3 book](https://leanpub.com/D3-Tips-and-Tricks)
+
+Level 1: Automatic Table and Date Search (Required)
+Create a basic HTML web page or use the index.html file provided (we recommend building your own custom page!).
+
+
+
+Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
+Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+[JavaScript Array filter() Method](https://www.w3schools.com/jsref/jsref_filter.asp)
+
+[JavaScript | Array filter()](https://www.geeksforgeeks.org/javascript-array-filter/)
+
+[Learn map, filter and reduce in Javascript](https://medium.com/@joomiguelcunha/learn-map-filter-and-reduce-in-javascript-ea59009593c4)
+> The perfect toolset for your venture in Functional Programming
 
 ## Read in d3 json string ##
 * [Data Loading in D3](https://www.tutorialsteacher.com/d3js/loading-data-from-file-in-d3js)
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,56 @@
-// from data.js
-var tableData = data;
+// October 30, 2019
+// Jadd Cheng
 
-// YOUR CODE HERE!
+// Define data from data.js
+const tableData = data;
+
+// Get reference to table body.
+let tbody = d3.select("tbody");
+
+// Populate the site with the whole data table.
+data.forEach((ufoReport) => {
+    const row = tbody.append("tr");
+    for (key in ufoReport){
+        const cell = tbody.append("td");
+        cell.text(ufoReport[key]);
+    }
+});
+
+// Select the filter button.
+const filterBtn = d3.select("#filter-btn");
+
+// Event handler
+filterBtn.on("click", function(){
+    
+    // Prevent page from refreshing
+    // Select the input element and get the raw HTML node
+    
+    // grab references to the input element and the output div
+    // const inputElement = d3.select("#patient-form-input");
+    const inputElement = d3.select("#datetime.form-control");
+
+    // Get the value property of the input element, i.e. date.
+    // const inputValue = inputElement.property("value");
+    const inputDate = inputElement.property("value");
+
+    // console
+    console.log(inputDate);
+
+    // Filter data for objects with matching date.
+    const filteredData = data.filter(data => data.datetime == inputDate);
+
+    // console display filterData array of objects
+    console.log(filteredData);
+    
+    // Redraw/update table
+    
+    // remove? --> should be remove everything after tbody.
+    filteredData.forEach((ufoReport) => {
+        const row = tbody.append("tr");
+        for (key in ufoReport){
+            const cell = tbody.append("td");
+            cell.text(ufoReport[key]);
+            // row.append("td").text(weatherReport[key]);
+        }
+    });
+});
